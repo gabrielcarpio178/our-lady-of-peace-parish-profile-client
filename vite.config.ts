cc52eefa5e8a1b7279403779ae8938153cd2eac7
gabrielcarpio178 <gabrielcarpio178@gmail.com>
2025-06-07 02:31:51 +0800

    07/06/2025

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/our-lady-of-peace-parish-profile-client",
 })
