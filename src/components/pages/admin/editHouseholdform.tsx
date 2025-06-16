2d4cb215c7c77c83667ad382710d7a296464db94
gabrielcarpio178 <gabrielcarpio178@gmail.com>
2025-06-17 01:52:58 +0800

    17/06/2025

--- src/components/pages/admin/editHouseholdform.tsx
+++ src/components/pages/admin/editHouseholdform.tsx
@@ -57,7 +57,7 @@ export default function EditSurveyForm(){
             setbec_id(user.bec_id ?? "")
             setattendants(user.mass_attendants ?? "");
             setbaptism(user.baptism ?? "")
-            setconfirmation(user.confirmation)
+            setconfirmation(user.isNotBaptismConfirmation)
             setmarriage(user.marrige ?? "")
             setprofessional(user.no_professional ?? "")
             sethigh_school(user.no_high_school ?? "")
