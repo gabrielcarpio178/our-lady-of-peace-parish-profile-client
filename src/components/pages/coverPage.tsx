cfd566c6ea16a81473792d94ce845f81899e9910
gabrielcarpio178 <gabrielcarpio178@gmail.com>
2025-06-17 13:52:05 +0800

    17/06/2025

--- src/components/pages/coverPage.tsx
+++ src/components/pages/coverPage.tsx
@@ -6,59 +6,98 @@ import john from './../../assets/image/fr.johnrey.png'
 import React, { useEffect, useState } from 'react';
 import { FaSquareFacebook } from "react-icons/fa6";
 import { IconContext } from 'react-icons';
+import 'animate.css';
 
 export default function CoverPage(){
-    const [datacontent, setDataContent] = useState<ContentData[]>([])
+
+    const [img_id, setimg_id] = useState(2);
+    const [animationContent, setAnimationContent] = useState("animate__fadeInRight");
+    const [numbers, setNumbers] = useState(2)
 
     const content = [
         {img: nitodel, position: "Parish Priest" ,name: "Rev. Fr. Nitodel A. Soriano SThL, PhD"},
         {img: john, position: "Parochial Vicar" ,name: "Rev. Fr. John Rey C. Maquilan"},
     ]
 
-    useEffect(()=>{
-        setDataContent(content)
-    },[])
+    const handleSetImgId = (id: number) => {
+        setAnimationContent("animate__fadeOutLeft");
+        void setTimeout(() => {
+            setimg_id(id);
+            setAnimationContent("animate__fadeInRight");
+        }, 100);
+        setNumbers(id)
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNumbers(prevNumbers => {
+                const newNumber = (prevNumbers + 1) % 3;
+                setTimeout(() => handleSetImgId(newNumber), 0);
+                return newNumber;
+            });
+        }, 3000);
+    return () => clearInterval(interval);
+}, []);
+
 
     return (
         <>
-            <div className='bg-[#44618E] w-full h-screen before:bg-[#00447880] before:content-[""] before:w-full before:h-screen relative before:absolute before:opacity-50 before:z-30'>
+            <div className='bg-[#44618E] w-full h-screen before:bg-[#00447880] before:content-[""] before:w-full before:h-screen relative before:absolute before:opacity-50 before:z-30 flex flex-col justify-between'>
                 <img src={churchImg} alt="churchImg" className='w-full h-full absolute z-20' />
 
-                <div className='p-14 absolute z-[999] w-full flex flex-row justify-between items-center'>
-                    <div className='text-white w-[40%] text-5xl font-serif'>
-                        Our Lady of Peace Parish Diocese of Bacolod
-                    </div>
-                    <div className=''>
-                        <Link to="/login" className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Login</Link>
+                <div className='static z-[999]'>
+                    <div className='p-14 w-full flex flex-row justify-between items-center'>
+                        <div className='text-white w-[40%] text-5xl font-serif'>
+                            Our Lady of Peace Parish Diocese of Bacolod
+                        </div>
+                        <div className=''>
+                            <Link to="/login" className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Login</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        
-            <div className=' w-full h-screen overflow-hidden relative'>
-                <img src={coverpage_bg} alt="coverpage background" className='w-full object-center absolute -top-96' />
-
-                <div className='absolute z-[999] grid grid-cols-1 h-full w-full'>
-                    {datacontent.map((data: any)=>{
-                        return(
-                            <ContentCard img={data.img} position={data.position} name={data.name}/>
-                        )
-                    })}
-                </div>
-
-            </div>
-
-            <div className='flex flex-row items-center gap-x-2 bg-[#001656] text-white p-1'>
-                <div>
-                    <IconContext.Provider value={{ color: "white", size: "2em" }}>
-                        <FaSquareFacebook/>
-                    </IconContext.Provider>
+                <div className='static z-[999] overflow-hidden'>
+                    {img_id!==2&&
+                        <div className={`bg-center bg-no-repeat bg-cover py-5 animate__animated ${animationContent}`} style={{ backgroundImage: `url('${coverpage_bg}')`}}>
+                            <div className='grid grid-cols-1 h-full w-full'>
+                                <ContentCard img={content[img_id].img} position={content[img_id].position} name={content[img_id].name}/> 
+                            </div>
+                        </div>
+                    }
                     
                 </div>
-                <div className='text-lg'>
-                    Our Lady of Peace Parish - Diocese of Bacolod
+
+                <div className='static z-[999]'>
+                    <div className='w-full flex flex-col items-center gap-5'>
+                        <div className='flex flex-row gap-5'>
+                            <div className={`${img_id!=2?"bg-gray-400":"bg-black"}  opacity-50 cursor-pointer w-4 h-4 rounded-full`} onClick={()=>handleSetImgId(2)}></div>
+                            <div className={`${img_id!=0?"bg-gray-400":"bg-black"} opacity-50 cursor-pointer w-4 h-4 rounded-full`} onClick={()=>handleSetImgId(0)}></div>
+                            <div className={`${img_id!=1?"bg-gray-400":"bg-black"} opacity-50 cursor-pointer w-4 h-4 rounded-full`} onClick={()=>handleSetImgId(1)}></div>
+                        </div>
+                        <div className='flex flex-row items-center gap-x-2 bg-[#001656] text-white p-1 w-full'>
+                            <div>
+                                <IconContext.Provider value={{ color: "white", size: "2em" }}>
+                                    <FaSquareFacebook/>
+                                </IconContext.Provider>
+                                
+                            </div>
+                            <div className='text-lg'>
+                                Our Lady of Peace Parish - Diocese of Bacolod
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
                 
+
+                {/* 
+
+                
+
+                 */}
+                
             </div>
+
+            
 
         </>
     )
@@ -74,21 +113,27 @@ const ContentCard:React.FC<ContentData> = (props) =>{
     const {img, position, name} = props
     return (
         <>
-            <div className='flex items-center'>
-                <div className='flex flex-row items-center w-full justify-center gap-x-10'>
-                    <div className='w-[20%] overflow-hidden flex items-center justify-center'>
-                        <img src={img} alt={name} className='w-full object-cover' />
-                    </div>
-                    <div className='w-[60%]'>
-                        <div className='text-5xl font-serif'>
-                            {name}
+            
+                    <div className='flex items-center'>
+                        <div className='flex flex-row items-center w-full justify-center gap-x-10'>
+                            <div className='w-[20%] overflow-hidden flex items-center justify-center'>
+                                <img src={img} alt={name} className='w-full object-cover' />
+                            </div>
+                            <div className='w-[60%]'>
+                                <div className='text-5xl font-serif'>
+                                    {name}
+                                </div>
+                                <div className='text-3xl font-semibold opacity-[71%]'>
+                                    {position}
+                                </div>
+                            </div>
                         </div>
-                        <div className='text-3xl font-semibold opacity-[71%]'>
-                            {position}
-                        </div>
                     </div>
-                </div>
-            </div>
+                
         </>
     )
+}
+
+function useRef<T>(arg0: null) {
+    throw new Error('Function not implemented.');
 }
